0da19668f3ec1e5932a10d84920f59a3f273c740
Adryel <adryxd003@gmail.com>
2026-06-18 16:18:56 -0300

    feat: adiciona dominio do modulo user

---
+++ src/modules/user/domain/repository/UserRepository.js
@@ -0,0 +1,16 @@
+class UserRepository {
+    
+  async create(user) {
+    throw new Error('Not implemented');
+  }
+ 
+  async findByEmail(email) {
+    throw new Error('Not implemented');
+  }
+ 
+  async findById(id) {
+    throw new Error('Not implemented');
+  }
+}
+ 
+export default UserRepository;
