afe2a2594f875b95e6a8c6e03db13c95b4b7aeb3
Adryel <sapelliadryel@gmail.com>
2026-06-18 21:25:25 -0300

    chore: adiciona dependências de Prisma ORM

--- .history/src/web/routes/address/sharedRouter_20260618201424.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import addressController from "../../../modules/address/application/controller/AddressController.js";
- 
-const router = Router();
- 
-router.get("/:token", addressController.getShared);
- 
-export default router;
--- .history/src/web/routes/address/sharedRouter_20260618201435.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import addressController from "../../../modules/address/";
- 
-const router = Router();
- 
-router.get("/:token", addressController.getShared);
- 
-export default router;
