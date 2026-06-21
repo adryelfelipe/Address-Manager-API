e35b5fa0a3d72e2ce1cf86377d2b8891bf3ac01e
Adryel Felipe <adryxd003@gmail.com>
2026-06-21 15:39:35 -0300

    feat: adiciona classe de erros padronizados com padrão ProblemDetails

---
+++ src/shared/infrastructure/errors/ProblemDetail.js
@@ -0,0 +1,11 @@
+class ProblemDetails {
+  constructor(type, title, status, detail, instance) {
+    this.type = type;
+    this.title = title;
+    this.status = status;
+    this.detail = detail;
+    this.instance = instance;
+  }
+}
+
+export default ProblemDetails;
